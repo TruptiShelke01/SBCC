c690e02fae75d4e5abe2d48c97fdfd28d526523a
krishnashinde30 <krishnashinde7404@gmail.com>
2024-09-26 09:38:22 +0530

    INitialized

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,11 @@
+<footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <p><i class="fa fa-copyright"></i> Copyright 2024 by Extreme Spoken English Training Centre   
+          
+           | Dev: <a href="www.digitechbranding.in" rel="sponsored" target="_parent">digitechbranding.in</a></p>
+        </div>
+      </div>
+    </div>
+  </footer>
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,5 @@
+@include('layouts.header')
+
+@yield('main-content')
+
+@include('layouts.footer')
